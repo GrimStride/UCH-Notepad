15c7d8a13aa09c488fbff8265f1502ec36923a87
GrimStride <63607052+GrimStride@users.noreply.github.com>
2020-12-17 02:50:05 -0500

    Decompression implemented
    Overwrites all files, method for keeping old config.json needed

--- updater.nim
+++ updater.nim
@@ -1,11 +1,14 @@
 #Based on rockyourcode.com readme.md template tutorial
-import httpClient, os, terminal, system
-from os import fileExists
+import httpClient, os, terminal, system, zip/zipfiles
+from os import fileExists, getCurrentDir
 from terminal import getch
 from system import quit
 
+{.passl: "-lz".}
+
 var url = "https://github.com/GrimStride/UCH-Notepad/releases/download/1.2/uchnotepad-v1.2-win64.zip"
 var check = fileExists("uchnotepad.exe")
+var current = getCurrentDir()
 
 proc downloadUpdate(link: string) =
   var client = newHttpClient()
@@ -17,9 +20,15 @@ proc downloadUpdate(link: string) =
     echo("Update downloaded succesfully.")
   except IOError as err:
     echo("Error: " & err.msg)
+    return
+  var z: ZipArchive
+  if not z.open("uchnotepad-v1.2-win64.zip"):
+    echo("Opening zip failed")
+    quit(1)
+  z.extractAll(current)
 
 when isMainModule:
-  echo("UCH Notepad Updater v0.1 by Grim Stride")
+  echo("UCH Notepad Updater v0.2 by Grim Stride")
   if check == true:
     downloadUpdate(url)
   else:
