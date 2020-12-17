d1e3cb2513d268be35542cfb18a56295b25407ca
GrimStride <63607052+GrimStride@users.noreply.github.com>
2020-12-17 17:18:32 -0500

    Main functionality completed
    -Only updates the tool to version 1.2

--- updater.nim
+++ updater.nim
@@ -1,6 +1,6 @@
 #Based on rockyourcode.com readme.md template tutorial
 import httpClient, os, terminal, system, zip/zipfiles
-from os import fileExists, getCurrentDir
+from os import fileExists, dirExists, getCurrentDir, moveFile, removeFile, removeDir
 from terminal import getch
 from system import quit
 
@@ -8,6 +8,7 @@ from system import quit
 
 var url = "https://github.com/GrimStride/UCH-Notepad/releases/download/1.2/uchnotepad-v1.2-win64.zip"
 var check = fileExists("uchnotepad.exe")
+var prev = dirExists("lib/urllib3")
 var current = getCurrentDir()
 
 proc downloadUpdate(link: string) =
@@ -17,20 +18,38 @@ proc downloadUpdate(link: string) =
     var file = open("uchnotepad-v1.2-win64.zip", fmWrite)
     defer: file.close()
     file.write(client.getContent(link))
-    echo("Update downloaded succesfully.")
+    echo("Update downloaded succesfully")
   except IOError as err:
     echo("Error: " & err.msg)
     return
+  echo("Backing up user preferences...")
+  moveFile("config.json", "config.json1")
+  removeDir("lib")
+  echo("User preferences backed up")
   var z: ZipArchive
   if not z.open("uchnotepad-v1.2-win64.zip"):
     echo("Opening zip failed")
     quit(1)
+  echo ("Extracting update files...")
   z.extractAll(current)
+  echo("Extraction completed")
+  echo("Restoring user preferences...")
+  removeFile("config.json")
+  moveFile("config.json1", "config.json")
+  echo("User preferences restored")
+  echo("Cleaning up...")
+  z.close()
+  removeFile("uchnotepad-v1.2-win64.zip")
+  echo("DONE")
+  echo("UCH Notepad has been updated to version 1.2 succesfully")
 
 when isMainModule:
-  echo("UCH Notepad Updater v0.2 by Grim Stride")
+  echo("UCH Notepad Updater v0.5 by Grim Stride")
   if check == true:
-    downloadUpdate(url)
+    if prev == true:
+      downloadUpdate(url)
+    else:
+      echo("UCH Notepad 1.2 or greater is already installed")
   else:
     echo("Error: UCH Notepad executable not found, please move this file to the folder containing UCH Notepad's executable")
   echo("Press any key to exit...")
